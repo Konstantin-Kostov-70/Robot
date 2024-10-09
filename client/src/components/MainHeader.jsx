777712a1fe7d2b20deebd7f61e809fdc36bc68af
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-10-09 16:41:16 +0200

    Create MainHeader component

--- client/src/components/MainHeader.jsx
+++ client/src/components/MainHeader.jsx
@@ -1,58 +1,15 @@
-const MainHeader = () => {
+/* eslint-disable react/prop-types */
+import Nav from "./Nav";
+
+const MainHeader = ({ introContent }) => {
   return (
     <>
       <section id="header" className="main-header">
         <div className="container">
-          <div className="row">
-            <nav className="navbar navbar-default">
-              <div className="navbar-header">
-                <button
-                  type="button"
-                  className="navbar-toggle collapsed"
-                  data-toggle="collapse"
-                  data-target="#site-nav-bar"
-                  aria-expanded="false"
-                >
-                  <span className="sr-only">Toggle navigation</span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                </button>
-              </div>
-
-              <div className="collapse navbar-collapse" id="site-nav-bar">
-                <ul className="nav navbar-nav">
-                  <li className="active">
-                    <a href="/">Home</a>
-                  </li>
-                  <li>
-                    <a href="/about">About</a>
-                  </li>
-                  <li>
-                    <a href="/shop">Shop</a>
-                  </li>
-                  <li>
-                    <a href="/faq">FAQ</a>
-                  </li>
-                  <li>
-                    <a href="/contacts">Contact</a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-          </div>
-
+          <Nav />
           <div className="intro row">
             <div className="overlay"></div>
-            <div className="col-sm-6 col-sm-offset-6">
-              <h2 className="header-quote">Save time and lower</h2>
-              <p>Your sweeping costs with the</p>
-              <h1 className="header-title">
-                Robot
-                <br/>
-                  <span className="thin">Factory</span>
-              </h1>
-            </div>
+            { introContent }
           </div>
         </div>
         <div className="nutral"></div>
