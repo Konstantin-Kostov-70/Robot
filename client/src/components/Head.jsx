95cb0238df4e8d6118a682745e7a5f70c6a6d06a
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-10-09 14:17:00 +0200

    create Head component

---
+++ client/src/components/Head.jsx
@@ -0,0 +1,33 @@
+import { Helmet } from "react-helmet-async";
+
+const Head = () => {
+  return (
+    <Helmet>
+      <meta charset="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Robot</title>
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto:400,300,500,700"
+        rel="stylesheet"
+        type="text/css"
+      />
+      <link
+        href="https://fonts.googleapis.com/css?family=Oswald:400,300,700"
+        rel="stylesheet"
+        type="text/css"
+      />
+      <link href="/static/css/bootstrap.min.css" rel="stylesheet" />
+      <link href="/static/css/magnific-popup.css" rel="stylesheet" />
+      <link href="/static/css/owl.carousel.css" rel="stylesheet" />
+      <link href="/static/css/owl.carousel.theme.min.css" rel="stylesheet" />
+      <link href="/static/css/ionicons.css" rel="stylesheet" />
+      <link href="/static/css/main.css" rel="stylesheet" />
+
+      {/* <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> */}
+    </Helmet>
+  );
+};
+
+export default Head;
