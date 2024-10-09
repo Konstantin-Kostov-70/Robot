7849913af8116b5daeada7ff84169141066ccdbe
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-10-09 16:40:40 +0200

    Create History component

---
+++ client/src/components/History.jsx
@@ -0,0 +1,40 @@
+const History = () => {
+  return (
+    <>
+      <section id="history" className="history">
+        <div className="container section-bg">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="title-box">
+                <p>Since 1990</p>
+                <h2 className="title mt0">Discover our history</h2>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="boxed">
+              <div className="col-sm-10 col-sm-offset-1">
+                <p>
+                  Lorem ipsum dolor sit amet consectetuer adipiscing elit sed
+                  diam nonummynibh euismod tincidunt ut laoree Dolore magna
+                  aliquam erat volutpat.Lorem ipsum dolor sit amet consectetuer
+                  adipiscing elit sed diam nonummynibh euismod tincidunt ut
+                  laoree Dolore magna aliquam erat .
+                </p>
+              </div>
+              <div className="col-sm-12">
+                <img
+                  className="img-responsive"
+                  src="/static/images/history.png"
+                  alt="history"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default History;
