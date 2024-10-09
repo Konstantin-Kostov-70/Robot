9a08593e4240fae9b7c93b3c29d984a53a00d5bf
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-10-09 16:39:54 +0200

    added components to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,20 @@ import Head from "../components/Head";
 import SiteHeader from "../components/SiteHeader";
 import Footer from "../components/Footer";
 import MainHeader from "../components/MainHeader";
+import Product from "../components/Product";
+import IntroHome from "../components/IntroHome";
+import History from "../components/History";
+import Partner from "../components/Partner";
 
 const Home = () => {
   return (
     <div>
       <Head />
       <SiteHeader />
-      <MainHeader />
+      <MainHeader introContent={<IntroHome />} />
+      <Product />
+      <History />
+      <Partner />
       <h1 id="home">Home Page</h1>
       <Footer />
     </div>
