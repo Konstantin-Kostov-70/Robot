f513d6d1d4f5a7285593c5351d165a8dfa0d9871
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-10-09 16:42:07 +0200

    Created Partner  component

---
+++ client/src/components/Partner.jsx
@@ -0,0 +1,64 @@
+const Partner = () => {
+  return (
+    <>
+     <section id="partner" className="partner">
+            <div className="container section-bg">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <div className="title-box">
+                            <p>Our media</p>
+                            <h2 className="title mt0">Partner</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="boxed">
+                       <div className="col-sm-12">
+                           <div id="partner-slider" className="owl-carousel">
+                                <div className="item">
+                                    <img src="/static/images/6.png" alt="partner" />
+                                </div>
+                                <div className="item">
+                                    <img src="/static/images/7.png" alt="partner" />
+                                </div>
+                                <div className="item">
+                                    <img src="/static/images/8.png" alt="partner" />
+                                </div>
+                                <div className="item">
+                                    <img src="/static/images/9.png" alt="partner" />
+                                </div>
+                                <div className="item">
+                                    <img src="/static/images/6.png" alt="partner" />
+                                </div>
+                                <div className="item">
+                                    <img src="/static/images/7.png" alt="partner" />
+                                </div>
+                                <div className="item">
+                                    <img src="/static/images/8.png" alt="partner" />
+                                </div>
+                                <div className="item">
+                                    <img src="/static/images/9.png" alt="partner" />
+                                </div>
+                                <div className="item">
+                                    <img src="/static/images/6.png" alt="partner" />
+                                </div>
+                                <div className="item">
+                                    <img src="/static/images/7.png" alt="partner" />
+                                </div>
+                                <div className="item">
+                                    <img src="/static/images/8.png" alt="partner" />
+                                </div>
+                                <div className="item">
+                                    <img src="/static/images/9.png" alt="partner" />
+                                </div>
+                            </div>
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Partner
