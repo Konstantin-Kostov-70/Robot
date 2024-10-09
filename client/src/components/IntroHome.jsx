93a6c0696318656e77a0d2e04278243a3947554e
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-10-09 16:41:01 +0200

    Create IntroHome component

---
+++ client/src/components/IntroHome.jsx
@@ -0,0 +1,15 @@
+const IntroHome = () => {
+  return (
+    <div className="col-sm-6 col-sm-offset-6">
+      <h2 className="header-quote">Save time and lower</h2>
+      <p>Your sweeping costs with the</p>
+      <h1 className="header-title">
+        Robot
+        <br />
+        <span className="thin">Factory</span>
+      </h1>
+    </div>
+  );
+};
+
+export default IntroHome;
