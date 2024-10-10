f41356570614ec7301914cdff3cdba41ca3af6dc
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-10-10 23:25:44 +0200

    Created IntroAbout.jsx component

---
+++ client/src/components/IntroAbout.jsx
@@ -0,0 +1,14 @@
+const IntroAbout = () => {
+  return (
+    <div className="col-sm-12">
+      <ol className="breadcrumb">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li className="active">About</li>
+      </ol>
+    </div>
+  );
+};
+
+export default IntroAbout;
