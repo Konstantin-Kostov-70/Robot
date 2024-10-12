611b3115eee7c59aa7ed2ae71bf2e1ddc6d5a2d0
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-10-12 15:25:29 +0200

    Added content to Shop.jsx

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -1,8 +1,203 @@
+import Head from '../components/Head'
+import SiteHeader from '../components/SiteHeader'
+import MainHeader  from '../components/MainHeader'
+import IntroAbout from '../components/IntroAbout'
+import Footer from '../components/Footer'
 const Shop = () => {
   return (
-    <div>
-      <h1>Shop</h1>
-    </div>
+    <>
+      <Head title={'Robot | Shop'} />
+      <SiteHeader />
+      <MainHeader introContent={<IntroAbout link={'Shop'} />} additionalClasses="shop-header inner-header" />
+      <section className="shop">
+            <div className="container page-bgc">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <div className="title-box">
+                            <p>Get our</p>
+                            <h2 className="title mt0">Products</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="boxed">
+                        <div className="col-sm-6">
+                            <div className="shop-box">
+                                <img className="img-full img-responsive" src="/static/images/shop-1.png" alt="shop" />
+                                <div className="shop-box-hover text-center">
+                                    <div className="c-table">
+                                        <div className="c-cell">
+                                            <a href="#">
+                                                <span className="ion-heart"></span>
+                                            </a>
+                                            <a href="#">
+                                                <span className="ion-ios-search-strong"></span>
+                                            </a>
+                                            <a href="#">
+                                                <span className="ion-ios-cart"></span>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="shop-box-title">
+                                <div className="row">
+                                    <div className="col-sm-6">
+                                        <h4>Industry Robot</h4>
+                                        <h4>MO: <span className="thin">#00SS5</span></h4>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <p className="shop-price">
+                                            $ 747
+                                        </p>
+                                        <div className="star">
+                                            <span className="ion-ios-star"></span>
+                                            <span className="ion-ios-star"></span>
+                                            <span className="ion-ios-star"></span>
+                                            <span className="ion-ios-star-half"></span>
+                                            <span className="ion-ios-star-outline"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="shop-box">
+                                <img className="img-full img-responsive" src="/static/images/shop-2.png" alt="shop" />
+                                <div className="shop-box-hover text-center">
+                                    <div className="c-table">
+                                        <div className="c-cell">
+                                            <a href="#">
+                                                <span className="ion-heart"></span>
+                                            </a>
+                                            <a href="#">
+                                                <span className="ion-ios-search-strong"></span>
+                                            </a>
+                                            <a href="#">
+                                                <span className="ion-ios-cart"></span>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="shop-box-title">
+                                <div className="row">
+                                    <div className="col-sm-6">
+                                        <h4>Industry Robot</h4>
+                                        <h4>MO: <span className="thin">#00SS5</span></h4>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <p className="shop-price">
+                                            $ 747
+                                        </p>
+                                        <div className="star">
+                                            <span className="ion-ios-star"></span>
+                                            <span className="ion-ios-star"></span>
+                                            <span className="ion-ios-star"></span>
+                                            <span className="ion-ios-star-half"></span>
+                                            <span className="ion-ios-star-outline"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="shop-box">
+                                <img className="img-full img-responsive" src="/static/images/shop-3.png" alt="shop" />
+                                <div className="shop-box-hover text-center">
+                                    <div className="c-table">
+                                        <div className="c-cell">
+                                            <a href="#">
+                                                <span className="ion-heart"></span>
+                                            </a>
+                                            <a href="#">
+                                                <span className="ion-ios-search-strong"></span>
+                                            </a>
+                                            <a href="#">
+                                                <span className="ion-ios-cart"></span>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="shop-box-title">
+                                <div className="row">
+                                    <div className="col-sm-6">
+                                        <h4>Industry Robot</h4>
+                                        <h4>MO: <span className="thin">#00SS5</span></h4>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <p className="shop-price">
+                                            $ 747
+                                        </p>
+                                        <div className="star">
+                                            <span className="ion-ios-star"></span>
+                                            <span className="ion-ios-star"></span>
+                                            <span className="ion-ios-star"></span>
+                                            <span className="ion-ios-star-half"></span>
+                                            <span className="ion-ios-star-outline"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="shop-box">
+                                <img className="img-full img-responsive" src="/static/images/shop-4.png" alt="shop" />
+                                <div className="shop-box-hover text-center">
+                                    <div className="c-table">
+                                        <div className="c-cell">
+                                            <a href="#">
+                                                <span className="ion-heart"></span>
+                                            </a>
+                                            <a href="#">
+                                                <span className="ion-ios-search-strong"></span>
+                                            </a>
+                                            <a href="#">
+                                                <span className="ion-ios-cart"></span>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="shop-box-title">
+                                <div className="row">
+                                    <div className="col-sm-6">
+                                        <h4>Industry Robot</h4>
+                                        <h4>MO: <span className="thin">#00SS5</span></h4>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <p className="shop-price">
+                                            $ 747
+                                        </p>
+                                        <div className="star">
+                                            <span className="ion-ios-star"></span>
+                                            <span className="ion-ios-star"></span>
+                                            <span className="ion-ios-star"></span>
+                                            <span className="ion-ios-star-half"></span>
+                                            <span className="ion-ios-star-outline"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-12">
+                            <nav>
+                                <ul className="pager">
+                                    <li className="previous disabled"><a href="#"><span aria-hidden="true" className="ion-chevron-left"></span></a></li>
+                                    <li><a href="#">1</a></li>
+                                    <li><a href="#">2</a></li>
+                                    <li><a href="#">3</a></li>
+                                    <li className="next"><a href="#"><span aria-hidden="true" className="ion-chevron-right"></span></a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+      </section>
+      <Footer />
+    </>
   )
 }
 
