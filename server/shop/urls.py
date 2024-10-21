7216e4e4d073c701e476c08358acdd0df66e11b6
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-10-21 18:52:43 +0200

    create shop urls

--- server/shop/urls.py
+++ server/shop/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from shop.views import *
 
 urlpatterns = [
-   path('products/', get_products, name='index'), 
+   path('products/', get_products, name='products'), 
 ]
