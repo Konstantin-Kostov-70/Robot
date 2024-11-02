219bafed2b98b6d7251e1241df2b9c98284ef9b2
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-11-02 15:55:51 +0100

    added Login icon and change the style

--- client/src/components/SiteHeader.jsx
+++ client/src/components/SiteHeader.jsx
@@ -1,18 +1,26 @@
 import { Link } from 'react-router-dom'
+import { AiFillAndroid } from "react-icons/ai";
 
 const SiteHeader = () => {
   return (
     <div className="site-header-bg">
       <div className="container">
-        <div className="row">
+        <div className="header-row">
           <div className="col-sm-6">
             <Link to="/">
               <img src="/static/images/logo.png" alt="logo" />
             </Link>
           </div>
-          <div className="col-sm-3 col-sm-offset-3 text-right">
-            <span className="ion-android-cart"></span> 0 products
-            <form>
+          <div className="search">
+            <Link to='/login'>
+              <AiFillAndroid className='android' />
+            </Link>
+            <div className='cart-form-wrapper'>
+              <div className="cart-wrapper">
+                <span className="ion-android-cart cart"></span>
+                <span className='cart-number'>0 products</span>
+              </div>
+              <form>
               <div className="input-group">
                 <input type="text" className="form-control" placeholder="" />
                 <span className="input-group-btn">
@@ -21,7 +29,8 @@ const SiteHeader = () => {
                   </button>
                 </span>
               </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </div>
