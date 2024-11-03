ddd1266c4366d48c904d0ab166c82e20b891863e
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-11-03 01:24:16 +0100

    users path was added to urls

--- server/shop/urls.py
+++ server/shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from shop.views import *
+from .views import *
 
 urlpatterns = [
+   path('users/', get_users, name='users'),
    path('products/', get_products, name='products'), 
 ]
